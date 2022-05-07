719be1bb7120fe69d69023c2b7f391d6eea34ed8
yingkaining <ykn@zjut.edu.cn>
2022-05-07 13:13:02 +0800

    1

--- zjutcv/__init__.py
+++ zjutcv/__init__.py
@@ -1,2 +1,2 @@
 # Copyright (c) ZJUTCV. All rights reserved.
-from .image import *
+from .image import *  # noqa: F401, F403
--- zjutcv/image/__init__.py
+++ zjutcv/image/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) ZJUTCV. All rights reserved.
-from .colorspace import rgb2gray  # noqa: F401, F403
+from .colorspace import rgb2gray
 
 __all__ = [
     'rgb2gray',
