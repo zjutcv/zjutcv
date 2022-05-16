4b811dc81452c90ff4391f02a6f65a410e890614
yingkaining <kennying99@gmail.com>
2022-05-17 00:32:02 +0800

    Tue 17 May 2022 00:31:48 HKT +0800

--- zjutcv/utils/__init__.py
+++ zjutcv/utils/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) ZJUTCV. All rights reserved.
 from .time import print_time
+from .file import ZPath as Path
 
 __all__ = [
-    'print_time',
+    'print_time', 'Path'
 ]
---
+++ zjutcv/utils/file.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path, PosixPath, WindowsPath
+
+
+class ZPath(type(Path()), Path):
+    """
+    This class used to index 'Path' like a list.
+
+    There maybe something wrong with directly inheriting from Path due to __new__
+    in Path. Thanks to `Alex Deft` for his wonderful solution.
+    https://stackoverflow.com/a/62812595
+    """
+
+    def __getitem__(self, idx):
+        """
+        Index path component like a list.
+
+        Path: "DeskTop / dir1 / User / Game / CSGO.exe"
+                  0        1     2      3       4
+
+        Args:
+            idx (int): The index of component.
+
+        Returns:
+            Component string.
+        """
+        return self.parts[idx]
